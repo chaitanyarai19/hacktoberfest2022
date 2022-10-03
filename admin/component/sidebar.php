b891398e7a3b1e28550eb653e9e80c23d13b21b1
Manish Shambhoo Sah <msah74915@gmail.com>
2022-10-03 19:49:22 +0530

    bug: fixed direction

--- admin/component/sidebar.php
+++ admin/component/sidebar.php
@@ -6,17 +6,17 @@
         <ul class="metismenu" id="menu">
             <li class="nav-label">Dashboard</li>
             <li>
-                <a href="widgets.html" aria-expanded="false">
+                <a href="#" aria-expanded="false">
                     <i class="fa fa-plus menu-icon"></i><span class="nav-text">Create Coupon</span>
                 </a>
             </li>
             <li>
-                <a href="widgets.html" aria-expanded="false">
+                <a href="#" aria-expanded="false">
                     <i class="fa fa-info menu-icon"></i><span class="nav-text">Vendor Details</span>
                 </a>
             </li>
             <li>
-                <a href="widgets.html" aria-expanded="false">
+                <a href="change-password.php" aria-expanded="false">
                     <i class="fa fa-key menu-icon"></i><span class="nav-text">Change Password</span>
                 </a>
             </li>
