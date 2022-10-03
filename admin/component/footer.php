c7e16f301effa58a17e69b49c5227f804ee36192
Manish Shambhoo Sah <msah74915@gmail.com>
2022-10-03 19:45:00 +0530

    new: change-password.php page is added

--- admin/component/footer.php
+++ admin/component/footer.php
@@ -3,7 +3,7 @@
         ***********************************-->
 <div class="footer">
     <div class="copyright">
-        <p>Copyright &copy; Designed & Developed by <a href="https://themeforest.net/user/quixlab">Quixlab</a> 2018</p>
+        <p>Copyright &copy; CouponDay</p>
     </div>
 </div>
 <!--**********************************
